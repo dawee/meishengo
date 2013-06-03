8a32dd5ba4ec26dfefa1b923bfe3b36f3154b629
David Corticchiato <david.corticchiato@gmail.com>
2013-06-03 13:09:49 +0200

    loader (Sonic) version

--- public/js/meishengo.js
+++ public/js/meishengo.js
@@ -11342,6 +11342,337 @@ require.register("visionmedia-page.js/index.js", function(exports, require, modu
 })();
 
 });
+require.register("dawicorti-Sonic/src/sonic.js", function(exports, require, module){
+/*
+ * Sonic 0.2
+ * --
+ * https://github.com/padolsey/Sonic
+ * --
+ * This program is free software. It comes without any warranty, to
+ * the extent permitted by applicable law. You can redistribute it
+ * and/or modify it under the terms of the Do What The Fuck You Want
+ * To Public License, Version 2, as published by Sam Hocevar. See
+ * http://sam.zoy.org/wtfpl/COPYING for more details. */ 
+
+module.exports = (function(){
+
+	var emptyFn = function(){};
+
+	function Sonic(d) {
+
+		this.converter = d.converter;
+
+		this.data = d.path || d.data;
+		this.imageData = [];
+
+		this.multiplier = d.multiplier || 1;
+		this.padding = d.padding || 0;
+
+		this.fps = d.fps || 25;
+
+		this.stepsPerFrame = ~~d.stepsPerFrame || 1;
+		this.trailLength = d.trailLength || 1;
+		this.pointDistance = d.pointDistance || .05;
+
+		this.domClass = d.domClass || 'sonic';
+
+		this.backgroundColor = d.backgroundColor || 'rgba(0,0,0,0)';
+		this.fillColor = d.fillColor;
+		this.strokeColor = d.strokeColor;
+
+		this.stepMethod = typeof d.step == 'string' ?
+			stepMethods[d.step] :
+			d.step || stepMethods.square;
+
+		this._setup = d.setup || emptyFn;
+		this._teardown = d.teardown || emptyFn;
+		this._preStep = d.preStep || emptyFn;
+
+		this.width = d.width;
+		this.height = d.height;
+
+		this.fullWidth = this.width + 2*this.padding;
+		this.fullHeight = this.height + 2*this.padding;
+
+		this.domClass = d.domClass || 'sonic';
+
+		this.setup();
+
+	}
+
+	var argTypes = Sonic.argTypes = {
+		DIM: 1,
+		DEGREE: 2,
+		RADIUS: 3,
+		OTHER: 0
+	};
+
+	var argSignatures = Sonic.argSignatures = {
+		arc: [1, 1, 3, 2, 2, 0],
+		bezier: [1, 1, 1, 1, 1, 1, 1, 1],
+		line: [1,1,1,1]
+	};
+
+	var pathMethods = Sonic.pathMethods = {
+		bezier: function(t, p0x, p0y, p1x, p1y, c0x, c0y, c1x, c1y) {
+			
+		    t = 1-t;
+
+		    var i = 1-t,
+		        x = t*t,
+		        y = i*i,
+		        a = x*t,
+		        b = 3 * x * i,
+		        c = 3 * t * y,
+		        d = y * i;
+
+		    return [
+		        a * p0x + b * c0x + c * c1x + d * p1x,
+		        a * p0y + b * c0y + c * c1y + d * p1y
+		    ]
+
+		},
+		arc: function(t, cx, cy, radius, start, end) {
+
+		    var point = (end - start) * t + start;
+
+		    var ret = [
+		        (Math.cos(point) * radius) + cx,
+		        (Math.sin(point) * radius) + cy
+		    ];
+
+		    ret.angle = point;
+		    ret.t = t;
+
+		    return ret;
+
+		},
+		line: function(t, sx, sy, ex, ey) {
+			return [
+				(ex - sx) * t + sx,
+				(ey - sy) * t + sy
+			]
+		}
+	};
+
+	var stepMethods = Sonic.stepMethods = {
+		
+		square: function(point, i, f, color, alpha) {
+			this._.fillRect(point.x - 3, point.y - 3, 6, 6);
+		},
+
+		fader: function(point, i, f, color, alpha) {
+
+			this._.beginPath();
+
+			if (this._last) {
+				this._.moveTo(this._last.x, this._last.y);
+			}
+
+			this._.lineTo(point.x, point.y);
+			this._.closePath();
+			this._.stroke();
+
+			this._last = point;
+
+		}
+
+	}
+
+	Sonic.prototype = {
+		setup: function() {
+
+			var args,
+				type,
+				method,
+				value,
+				data = this.data;
+
+			this.canvas = document.createElement('canvas');
+			this._ = this.canvas.getContext('2d');
+
+			this.canvas.className = this.domClass;
+
+			this.canvas.height = this.fullHeight;
+			this.canvas.width = this.fullWidth;
+
+			this.points = [];
+
+			for (var i = -1, l = data.length; ++i < l;) {
+
+				args = data[i].slice(1);
+				method = data[i][0];
+
+				if (method in argSignatures) for (var a = -1, al = args.length; ++a < al;) {
+
+					type = argSignatures[method][a];
+					value = args[a];
+
+					switch (type) {
+						case argTypes.RADIUS:
+							value *= this.multiplier;
+							break;
+						case argTypes.DIM:
+							value *= this.multiplier;
+							value += this.padding;
+							break;
+						case argTypes.DEGREE:
+							value *= Math.PI/180;
+							break;
+					};
+
+					args[a] = value;
+
+				}
+
+				args.unshift(0);
+
+				for (var r, pd = this.pointDistance, t = pd; t <= 1; t += pd) {
+					
+					// Avoid crap like 0.15000000000000002
+					t = Math.round(t*1/pd) / (1/pd);
+
+					args[0] = t;
+
+					r = pathMethods[method].apply(null, args);
+
+					this.points.push({
+						x: r[0],
+						y: r[1],
+						progress: t
+					});
+
+				}
+
+			}
+
+			this.frame = 0;
+
+			if (this.converter && this.converter.setup) {
+				this.converter.setup(this);
+			}
+
+		},
+
+		prep: function(frame) {
+
+			if (frame in this.imageData) {
+				return;
+			}
+
+			this._.clearRect(0, 0, this.fullWidth, this.fullHeight);
+			this._.fillStyle = this.backgroundColor;
+			this._.fillRect(0, 0, this.fullWidth, this.fullHeight);
+
+			var points = this.points,
+				pointsLength = points.length,
+				pd = this.pointDistance,
+				point,
+				index,
+				frameD;
+
+			this._setup();
+
+			for (var i = -1, l = pointsLength*this.trailLength; ++i < l && !this.stopped;) {
+
+				index = frame + i;
+
+				point = points[index] || points[index - pointsLength];
+
+				if (!point) continue;
+
+				this.alpha = Math.round(1000*(i/(l-1)))/1000;
+
+				this._.globalAlpha = this.alpha;
+
+				if (this.fillColor) {
+					this._.fillStyle = this.fillColor;
+				}
+				if (this.strokeColor) {
+					this._.strokeStyle = this.strokeColor;
+				}
+
+				frameD = frame/(this.points.length-1);
+				indexD = i/(l-1);
+
+				this._preStep(point, indexD, frameD);
+				this.stepMethod(point, indexD, frameD);
+
+			} 
+
+			this._teardown();
+
+			this.imageData[frame] = (
+				this._.getImageData(0, 0, this.fullWidth, this.fullWidth)
+			);
+
+			return true;
+
+		},
+
+		draw: function() {
+			
+			if (!this.prep(this.frame)) {
+
+				this._.clearRect(0, 0, this.fullWidth, this.fullWidth);
+
+				this._.putImageData(
+					this.imageData[this.frame],
+					0, 0
+				);
+
+			}
+
+			if (this.converter && this.converter.step) {
+				this.converter.step(this);
+			}
+
+			if (!this.iterateFrame()) {
+				if (this.converter && this.converter.teardown) {
+					this.converter.teardown(this);
+					this.converter = null;
+				}
+			}
+
+		},
+
+		iterateFrame: function() {
+			
+			this.frame += this.stepsPerFrame;
+			
+			if (this.frame >= this.points.length) {
+				this.frame = 0;
+				return false;
+			}
+
+			return true;
+
+		},
+
+		play: function() {
+
+			this.stopped = false;
+
+			var hoc = this;
+
+			this.timer = setInterval(function(){
+				hoc.draw();
+			}, 1000 / this.fps);
+
+		},
+		stop: function() {
+
+			this.stopped = true;
+			this.timer && clearInterval(this.timer);
+
+		}
+	};
+
+	return Sonic;
+
+}());
+
+});
 require.register("proxy-client/index.js", function(exports, require, module){
 /*global window*/
 /*jslint nomen: true*/
@@ -11359,6 +11690,38 @@ exports.getInstance = function (name) {
 };
 
 
+});
+require.register("loader/index.js", function(exports, require, module){
+/*global window*/
+/*jslint nomen: true*/
+"use strict";
+
+var _ = require('underscore'),
+    $ = require('jquery'),
+    Sonic = require('Sonic');
+
+exports.start = function () {
+    exports.stop();
+    var height = parseInt($('header').height() * 0.3, 10),
+        width = 3 * height,
+        square = new Sonic({
+            width: width,
+            height: height,
+            fillColor: '#d6a692',
+            path: [
+                ['line', 0, 0, width, 0],
+                ['line', width, 0, width, height],
+                ['line', width, height, 0, height],
+                ['line', 0, height, 0, 0]
+            ]
+        });
+    square.play();
+    $('header').append(square.canvas);
+};
+
+exports.stop = function () {
+    $('header .sonic').remove();
+}
 });
 require.register("login-page/index.js", function(exports, require, module){
 /*global window*/
@@ -11392,7 +11755,8 @@ var _ = require('underscore'),
     $ = require('jquery'),
     template = require('./form-template'),
     page = require('page'),
-    proxyClient = require('proxy-client');
+    proxyClient = require('proxy-client'),
+    loader = require('loader');
 
 function LoginForm() {
     _.bindAll(this);
@@ -11406,6 +11770,8 @@ LoginForm.prototype.render = function () {
 
 LoginForm.prototype.onConnect = function (event) {
     event.preventDefault();
+    this.$el.animate({opacity: 0}, {duration: 800});
+    loader.start();
     this.login = $('.login', this.$el).val();
     this.password = $('.password', this.$el).val();
     this.proxyClient = proxyClient.createInstance('pandanet');
@@ -11414,11 +11780,7 @@ LoginForm.prototype.onConnect = function (event) {
 
 LoginForm.prototype.onAuthRequest = function () {
     this.proxyClient.emit('auth:sending', {login: this.login, password: this.password});
-    this.proxyClient.once('auth:success', this.onAuthSuccess);
-};
-
-LoginForm.prototype.onAuthSuccess = function () {
-    this.$el.animate({opacity: 0}, {duration: 2000, done: this.openHomePage});
+    this.proxyClient.once('auth:success', this.openHomePage);
 };
 
 LoginForm.prototype.openHomePage = function () {
@@ -11449,7 +11811,7 @@ var _ = require('underscore'),
 function TopMenu(currentPage, animated) {
     _.bindAll(this);
     this.currentPage = currentPage || null;
-    this.animationDuration = !animated ? 0 : 1000;
+    this.animationDuration = !animated ? 0 : 500;
 }
 
 TopMenu.prototype.enlightCurrent = function () {
@@ -11526,7 +11888,6 @@ var _ = require('underscore'),
     $ = require('jquery'),
     template = require('./template');
 
-
 function PlayerTag(player) {
     _.bindAll(this);
     this.player = player;
@@ -11574,7 +11935,8 @@ var _ = require('underscore'),
     $ = require('jquery'),
     template = require('./list-template'),
     proxyClient = require('proxy-client'),
-    PlayerTag = require('player-tag');
+    PlayerTag = require('player-tag'),
+    loader = require('loader');
 
 function PlayersList() {
     _.bindAll(this);
@@ -11585,22 +11947,23 @@ function PlayersList() {
 
 PlayersList.prototype.render = function () {
     this.$el = $(template);
+    loader.start();
     this.proxyClient.emit('players:list:request');
     this.proxyClient.once('players:list:success', this.onPlayersListSuccess);
     return this;
 };
 
-PlayersList.prototype.enlightColumn = function() {
+PlayersList.prototype.enlightColumn = function () {
     var index = 0,
         columnIndex = _.keys(this.columns)[this.columnSlot],
         column = this.columns[columnIndex];
     for (index = 0; index < column.length; index += 1) {
         column[index].$el.animate({opacity: 1}, {duration: 1000});
     }
-    _.delay(this.nextColumn, 500);
+    _.delay(this.nextColumn, 50);
 };
 
-PlayersList.prototype.nextColumn = function() {
+PlayersList.prototype.nextColumn = function () {
     this.columnSlot += 1;
     if (this.columnSlot < _.size(this.columns)) {
         this.enlightColumn();
@@ -11608,11 +11971,13 @@ PlayersList.prototype.nextColumn = function() {
 };
 
 PlayersList.prototype.onPlayersListSuccess = function (data) {
+    loader.stop();
     var areaNames = ['.players-area-top', '.players-area-right', '.players-area-bottom'],
         areaIndex = 0,
         $area = $(areaNames[areaIndex], this.$el);
     _.each(data.players, function (player) {
-        var playerTag = new PlayerTag(player);
+        var playerTag = new PlayerTag(player),
+            columnIndex;
         if (areaIndex < areaNames.length) {
             $area.append(playerTag.render().$el);
             if (playerTag.$el.position().top + playerTag.$el.height() > $area.height()) {
@@ -11622,7 +11987,7 @@ PlayersList.prototype.onPlayersListSuccess = function (data) {
                     $area = $(areaNames[areaIndex], this.$el);
                 }
             } else {
-                var columnIndex = 'col' + playerTag.$el.position().left;
+                columnIndex = 'col' + $('.name', playerTag.$el).offset().left;
                 if (!_.has(this.columns, columnIndex)) {
                     this.columns[columnIndex] = [];
                 }
@@ -11711,7 +12076,20 @@ require.alias("component-underscore/index.js", "login-page/deps/underscore/index
 
 require.alias("visionmedia-page.js/index.js", "login-page/deps/page/index.js");
 
+require.alias("dawicorti-Sonic/src/sonic.js", "login-page/deps/Sonic/src/sonic.js");
+require.alias("dawicorti-Sonic/src/sonic.js", "login-page/deps/Sonic/index.js");
+require.alias("dawicorti-Sonic/src/sonic.js", "dawicorti-Sonic/index.js");
+
 require.alias("proxy-client/index.js", "login-page/deps/proxy-client/index.js");
+
+require.alias("loader/index.js", "login-page/deps/loader/index.js");
+require.alias("component-jquery/index.js", "loader/deps/jquery/index.js");
+
+require.alias("component-underscore/index.js", "loader/deps/underscore/index.js");
+
+require.alias("dawicorti-Sonic/src/sonic.js", "loader/deps/Sonic/src/sonic.js");
+require.alias("dawicorti-Sonic/src/sonic.js", "loader/deps/Sonic/index.js");
+require.alias("dawicorti-Sonic/src/sonic.js", "dawicorti-Sonic/index.js");
 
 require.alias("home-page/index.js", "router/deps/home-page/index.js");
 require.alias("component-underscore/index.js", "home-page/deps/underscore/index.js");
@@ -11752,4 +12130,13 @@ require.alias("component-jquery/index.js", "player-tag/deps/jquery/index.js");
 require.alias("component-underscore/index.js", "player-tag/deps/underscore/index.js");
 
 require.alias("visionmedia-page.js/index.js", "player-tag/deps/page/index.js");
+
+require.alias("loader/index.js", "players-page/deps/loader/index.js");
+require.alias("component-jquery/index.js", "loader/deps/jquery/index.js");
+
+require.alias("component-underscore/index.js", "loader/deps/underscore/index.js");
+
+require.alias("dawicorti-Sonic/src/sonic.js", "loader/deps/Sonic/src/sonic.js");
+require.alias("dawicorti-Sonic/src/sonic.js", "loader/deps/Sonic/index.js");
+require.alias("dawicorti-Sonic/src/sonic.js", "dawicorti-Sonic/index.js");
 
