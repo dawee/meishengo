8866c93aa6d60bfac922e4b64a2ba5a13c8d8099
David <david.corticchiato@gmail.com>
2014-03-19 07:52:55 +0100

    generate player token

--- app.js
+++ app.js
@@ -16,6 +16,14 @@ app.get('/game/:id', function (req, res) {
   res.render('index', {id: req.params.id});
 });
 
+app.get('/game/:id/join/:color', function (req, res) {
+  var game = Game(req.params.id, io);
+  var data = game.joinRequest(req.params.color);
+  res.writeHead(data.code, {'Content-Type': 'application/json'});
+  res.write(JSON.stringify(data));
+  res.end();
+});
+
 io.sockets.on('connection', Game.listen);
 
 server.listen(8000);
