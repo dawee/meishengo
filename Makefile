85405d53060b641f356ca2caa19c834406ef33b0
David Corticchiato <david.corticchiato@gmail.com>
2014-03-21 09:43:07 +0100

    on the fly less & browserify build

--- Makefile
+++ Makefile
@@ -1,8 +1,2 @@
-client_src := $(wildcard lib/client/*.js)
-
-client:
-	@./node_modules/.bin/browserify -o public/koban-client.js ${client_src}
-
 run:
-	@./node_modules/.bin/watchify -o public/koban-client.js ${client_src} &
 	@./node_modules/.bin/nodemon --ignore lib/client app.js &
--- app.js
+++ app.js
@@ -2,6 +2,8 @@ var express = require('express');
 var http = require('http');
 var Game = require('./lib/server/game');
 var sio = require('socket.io');
+var expressLess = require('express-less');
+var browserifyExpress = require('browserify-express');
 
 var app = express();
 var server = http.createServer(app)
@@ -11,6 +13,13 @@ app.use(express.static(__dirname + '/public'));
 app.use(express.bodyParser());
 app.set('views', __dirname + '/view');
 app.set('view engine', 'jade');
+app.use('/less', expressLess(__dirname + '/lib/client/style', {compress: true}));
+app.use(browserifyExpress({
+    entry: __dirname + '/lib/client/boot.js',
+    watch: __dirname + '/lib/client',
+    mount: '/js/meishengo-client.js',
+    minify: true
+}));
 
 app.get('/game/:id', function (req, res) {
   var game = Game(req.params.id, io);
