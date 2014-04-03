d5b23ccf4c8ae418f9aca813bf7c0d8dbb8ec34d
David Corticchiato <david.corticchiato@gmail.com>
2014-04-03 23:32:07 +0200

    close #24 : end game allows to remove dead groups

--- test/goban.test.js
+++ test/goban.test.js
@@ -9,7 +9,7 @@ describe('Goban', function () {
     it('should set "foobar" to true', function () {
       var goban = new Goban({groups: [
         [{row: 9, col: 9, color: 'black'}]
-      ]});
+      ], editable: true});
 
       goban.toggleGroupMarkupAt(9, 9, 'foobar');
       assert.equal(true, goban.groups[0].foobar);
@@ -18,11 +18,26 @@ describe('Goban', function () {
    it('should toggle "foobar" to false', function () {
       var goban = new Goban({groups: [
         [{row: 9, col: 9, color: 'black'}]
-      ]});
+      ], editable: true});
 
       goban.toggleGroupMarkupAt(9, 9, 'foobar');
       goban.toggleGroupMarkupAt(9, 9, 'foobar');
       assert.equal(false, goban.groups[0].foobar);
+    });
+
+   it('should send "markup:foobar" event', function (done) {
+      var goban = new Goban({groups: [
+        [{row: 9, col: 9, color: 'black'}]
+      ], editable: true});
+
+      goban.on('markup:foobar', function (args) {
+        assert.equal(true, args.value);
+        assert.equal(9, args.row);
+        assert.equal(9, args.col);
+        done();
+      });
+
+      goban.toggleGroupMarkupAt(9, 9, 'foobar');
     });
 
   });
