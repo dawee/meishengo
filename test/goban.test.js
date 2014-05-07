cc66dcf32749af3f01d623c1c87246e5d575b034
David Corticchiato <david.corticchiato@gmail.com>
2014-05-07 14:57:33 +0200

    fill goban model and test

---
+++ test/goban.test.js
@@ -0,0 +1,48 @@
+var assert = require('assert');
+var _ = require('underscore');
+var Goban = require('../lib/model/goban');
+
+describe('Goban', function () {
+
+  describe('canPutStone()', function () {
+
+    it('should accept a new stone', function () {
+      var goban = new Goban({size: 19});
+
+      assert.equal(true, goban.canPutStone({row: 0, col: 0, color: 'black'}));
+    });
+
+    it('should refuse a stone outside of limits', function () {
+      var goban = new Goban(); // defaults to 19
+
+      assert.equal(false, goban.canPutStone({row: 19, col: 0, color: 'black'}));
+    });
+
+    it('should refuse a stone if position taken', function () {
+      var goban = new Goban({size: 19, groups: [
+        [{row: 0, col: 0, color: 'black'}]
+      ]});
+
+      assert.equal(false, goban.canPutStone({row: 0, col: 0, color: 'black'}));
+      assert.equal(false, goban.canPutStone({row: 0, col: 0, color: 'white'}));
+    });
+
+    it('should not add a stone to groups', function () {
+      var goban = new Goban({size: 19});
+
+      assert.equal(true, goban.canPutStone({row: 0, col: 0, color: 'black'}));
+      assert.equal(0, goban.get('groups').size());
+    });
+
+  });
+
+  describe('putStone()', function () {
+
+    it('should create a new group', function () {
+      var goban = new Goban({size: 19});
+
+      assert.equal(true, goban.putStone({row: 0, col: 0, color: 'black'}));
+      assert.equal(1, goban.get('groups').size());
+    });
+  });
+});
