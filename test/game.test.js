54007c80e438f13b7572d022bbc354bd2f4bae80
David Corticchiato <david.corticchiato@gmail.com>
2014-05-10 01:33:09 +0200

    serliazer for models and collections

--- test/game.test.js
+++ test/game.test.js
@@ -1,41 +1,60 @@
 var assert = require('assert');
 var _ = require('underscore');
 var Game = require('../lib/model/game');
-var Goban = require('../lib/model/goban');
+var fixtures = {
+  's19bCenter': require('./fixtures/game-19x19-center-black-group')
+};
 
 describe('Game', function () {
 
   describe('putStone()', function () {
 
     it('should accept a first black stone', function () {
-      var goban = new Goban({size: 19});
-      var game = new Game({goban: goban});
+      var game = new Game({goban: {size: 19}});
 
       assert.equal(true, game.putStone({row: 0, col: 0, color: 'black'}));
     });
 
     it('should refuse a second black stone', function () {
-      var goban = new Goban({size: 19});
-      var game = new Game({goban: goban});
+      var game = new Game({goban: {size: 19}});
 
       assert.equal(true, game.putStone({row: 0, col: 0, color: 'black'}));
       assert.equal(false, game.putStone({row: 1, col: 1, color: 'black'}));
     });
 
     it('should accept a second white stone', function () {
-      var goban = new Goban({size: 19});
-      var game = new Game({goban: goban});
+      var game = new Game({goban: {size: 19}});
 
       assert.equal(true, game.putStone({row: 0, col: 0, color: 'black'}));
       assert.equal(true, game.putStone({row: 1, col: 1, color: 'white'}));
     });
 
     it('should refuse a first white stone', function () {
-      var goban = new Goban({size: 19});
-      var game = new Game({goban: goban});
+      var game = new Game({goban: {size: 19}});
 
       assert.equal(false, game.putStone({row: 0, col: 0, color: 'white'}));
     });
 
   });
+
+
+  describe('serialize()', function () {
+
+    it('should be fully recursive', function () {
+      var game = new Game(fixtures.s19bCenter);
+      assert.equal(8, game.serialize().goban.groups[0].stones.length);
+    });
+
+    it('should be usable for reset', function () {
+      var game1 = new Game(fixtures.s19bCenter);
+      var game2 = new Game();
+
+      game2.set(game1.serialize());
+
+      assert.equal(8, game2.gbn().get('groups').at(0).get('stones').size());
+    });
+
+
+  });
+
 });
