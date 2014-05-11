0da603c6611371e6b8c453c9d4ce375f3c3a5085
David Corticchiato <david.corticchiato@gmail.com>
2014-05-11 13:26:35 +0200

    GameStore.exists helper

---
+++ test/gamestore.test.js
@@ -0,0 +1,64 @@
+var assert = require('assert');
+var _ = require('underscore');
+var unflatten = require('flat').unflatten;
+var GameStore = require('../lib/store/game');
+var fixtures = {
+  's19bCenter': require('./fixtures/game-19x19-center-black-group')
+};
+
+describe('GameStore', function () {
+
+  it('should save flatten data on Redis', function (done) {
+    var game = GameStore.create(_.extend({
+      id: 'my-game'
+    }, fixtures.s19bCenter));
+
+    game.save(null, {
+      success: function () {
+        GameStore.exists('my-game', function (err, exists) {
+          assert.equal(true, exists);
+          done();
+        });
+      },
+      error: function (model, err) {
+        throw err;
+      }
+    });
+  });
+
+  it('should not find this game' , function (done) {
+    GameStore.exists('i-dont-exist', function (err, exists) {
+      assert.equal(false, exists);
+      done();
+    });
+  });
+
+  it('should be usable for reset', function (done) {
+    var game = GameStore.create({id: 'my-game'});
+
+    game.fetch({
+      success: function () {
+        assert.equal(8, game.gbn().get('groups').at(0).get('stones').size());
+        done();
+      },
+      error: function (model, err) {
+        throw err;
+      }
+    });
+  });
+
+
+  it('should delete an existing game from Redis', function (done) {
+    var game = GameStore.create({id: 'my-game'});
+
+    game.destroy({
+      success: function () {
+        done();
+      },
+      error: function (model, err) {
+        throw err;
+      }
+    });
+  });
+
+});
