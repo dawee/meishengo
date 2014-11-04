fc742cc0d17bb22270b9032f39131356e72a5642
David Corticchiato <david.corticchiato@gmail.com>
2014-11-04 21:09:26 +0100

    GameStore become GameCache (store for db, cache for redis)

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ var express = require('express');
 var http = require('http');
 var socketio = require('./lib/io/server');
 var hat = require('hat');
-var GameStore = require('./lib/store/game');
+var GameCache = require('./lib/cache/game');
 
 
 /*
@@ -61,7 +61,7 @@ app.get(/^\/?new\-game\/?/, function (req, res) {
 app.get(/^\/(game|g)\/([\w\-]{3,16})$/, function (req, res) {
   var id = req.params[1];
 
-  GameStore.fetch(id, function (err, game) {
+  GameCache.fetch(id, function (err, game) {
     res.render('game', {
       title: conf.get('title'),
       js: debug ? '/mei/game.js' : '/mei/game.min.js', 
--- test/gamecache.test.js
+++ test/gamecache.test.js
@@ -1,21 +1,21 @@
 var assert = require('assert');
 var _ = require('underscore');
 var unflatten = require('flat').unflatten;
-var GameStore = require('../lib/store/game');
+var GameCache = require('../lib/cache/game');
 var fixtures = {
   's19bCenter': require('./fixtures/game-19x19-center-black-group')
 };
 
-describe('GameStore', function () {
+describe('GameCache', function () {
 
   it('should save flatten data on Redis', function (done) {
-    var game = GameStore.create(_.extend({
+    var game = GameCache.create(_.extend({
       id: 'my-game'
     }, fixtures.s19bCenter));
 
     game.save(null, {
       success: function () {
-        GameStore.fetch('my-game', function (err, model) {
+        GameCache.fetch('my-game', function (err, model) {
           assert.equal(true, !!model);
           done();
         });
@@ -27,14 +27,14 @@ describe('GameStore', function () {
   });
 
   it('should not find this game' , function (done) {
-    GameStore.fetch('i-dont-exist', function (err, model) {
+    GameCache.fetch('i-dont-exist', function (err, model) {
       assert.equal(false, !!model);
       done();
     });
   });
 
   it('should be usable for reset', function (done) {
-    var game = GameStore.create({id: 'my-game'});
+    var game = GameCache.create({id: 'my-game'});
 
     game.fetch({
       success: function () {
@@ -49,7 +49,7 @@ describe('GameStore', function () {
 
 
   it('should delete an existing game from Redis', function (done) {
-    var game = GameStore.create({id: 'my-game'});
+    var game = GameCache.create({id: 'my-game'});
 
     game.destroy({
       success: function () {
