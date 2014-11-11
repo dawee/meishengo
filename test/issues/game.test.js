f75d4e34cbfa6cf37f10d87885228af0e738d963
David <david.corticchiato@gmail.com>
2014-11-11 11:44:53 +0100

    close #53 : all games were refering to same players instances

---
+++ test/issues/game.test.js
@@ -0,0 +1,23 @@
+var assert = require('assert');
+var _ = require('underscore');
+var Game = require('../../lib/model/game');
+
+
+describe('Game', function () {
+
+  describe('issue #53', function () {
+
+    it('should create each game with empty seats', function () {
+      var game1 = new Game({goban: {size: 19}});
+      var game2 = new Game({goban: {size: 19}});
+
+      game1.get('white').set('present', true);
+      game1.get('black').set('present', true);
+
+      assert.equal(false, game2.get('white').get('present'));
+      assert.equal(false, game2.get('black').get('present'));
+    });
+
+  });
+
+})
